08d8c36468d4c19d731db5d9e8f429c3e8980837
sarah boo <srhboo@gmail.com>
2021-03-14 19:40:14 -0400

    first commit

---
+++ shader.frag
@@ -0,0 +1,170 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+uniform float u_beat;
+uniform float u_transition1;
+uniform float u_perc1;
+uniform float u_perc1b;
+uniform float u_perc1c;
+uniform float u_perc2;
+uniform float u_perc2b;
+uniform float u_perc2c;
+uniform float u_sibeat;
+
+vec2 createGrid( in vec2 st, in vec2 grid, out vec2 indices) {
+    
+    st *= grid;
+    
+    indices = floor(st);
+    st = fract(st);
+
+    return st;
+}
+
+float drawCircle(vec2 st, vec2 pos, float size) {
+    float result = distance(st, vec2(pos));
+    result = 1.0 - smoothstep(size - 0.2, size + 0.3, result);    
+    return result;
+}
+
+
+float drawRectangle(vec2 st, vec2 pos, vec2 size) {
+    float result = 1.0;
+    vec2 border = (pos - size) / 2.0;
+    
+    result = step(border.x, st.x);
+    result *= step(border.x, pos.x - st.x);
+    result *= step(border.y, st.y);
+    result *= step(border.y, pos.y -st.y);
+    return result;
+}
+
+vec2 rotate2D(vec2 _st, float _angle){
+    _st -= 0.5;
+    _st =  mat2(cos(_angle),-sin(_angle),
+                sin(_angle),cos(_angle)) * _st;
+    _st += 0.5;
+    return _st;
+}
+
+float random (in vec2 _st) {
+    return fract(sin(dot(_st.xy,
+                         vec2(12.9898,78.233)))*
+        43758.5453123);
+}
+
+// Based on Morgan McGuire @morgan3d
+// https://www.shadertoy.com/view/4dS3Wd
+float noise (in vec2 _st) {
+    vec2 i = floor(_st);
+    vec2 f = fract(_st);
+
+    // Four corners in 2D of a tile
+    float a = random(i);
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
+
+    vec2 u = f * f * (3.0 - 2.0 * f);
+
+    return mix(a, b, u.x) +
+            (c - a)* u.y * (1.0 - u.x) +
+            (d - b) * u.x * u.y;
+}
+
+float plot(vec2 st, float pct, float minus, float plus){
+return  smoothstep( pct-minus, pct, st.y) -
+        smoothstep( pct, pct+plus, st.y);
+}
+
+#define NUM_OCTAVES 5
+
+float fbm ( in vec2 _st) {
+    float v = 0.0;
+    float a = 0.5;
+    vec2 shift = vec2(100.0);
+    // Rotate to reduce axial bias
+    mat2 rot = mat2(cos(0.8), sin(0.4),
+                    -sin(0.6), cos(0.40));
+    for (int i = 0; i < NUM_OCTAVES; ++i) {
+        v += a * noise(_st);
+        _st = rot * _st * 2.0 + shift;
+        a *= 0.6;
+    }
+    return v;
+}
+
+void main() {
+    vec2 st = gl_FragCoord.xy/u_resolution.xy;
+    float st_noise = noise(st);
+
+    // this will zoom in when it is time for the transition
+    st *= 1. + (19. * u_transition1);
+    st -= vec2(u_transition1 * 12.);
+    vec3 color = vec3(0.0);
+
+    // from brownian motion example
+    vec2 q = vec2(0.);
+    q.x = fbm( st + 0.00*u_time);
+    q.y = fbm( st + vec2(1.0));
+
+    vec2 r = vec2(0.);
+    r.x = fbm( st + 1.0*q + vec2(1.7,9.2)+ 0.15*u_time );
+    r.y = fbm( st + 1.0*q + vec2(8.3,2.8)+ 0.126*u_time);
+
+    float f = fbm(st+r);
+
+    // shaping functions
+    float y = sin(st.x+u_time/4.)*2. + cos(st.y + u_time / 2.);
+    float z = sin(st.x+u_time/2.)*1.5 + cos(st.x + u_time / 1.5);
+    float pct = plot(st,y, 0.4, 0.2) + plot(st, z, 0.6, 0.3);
+
+    color = mix(color, vec3(0.9608, 0.7529, 0.0745), pct);
+
+    // circle that corresponds to bass beat (it will trigger its opacity)
+    float c = drawCircle(st, vec2(sin(u_time / 6.) * 0.4 + 0.6, cos(u_time / 5.) * 0.4 + 0.6), 0.5 + u_transition1 * 10.5);
+    color = mix(color, vec3(0.0627, 0.3882, 0.3176), c*3. * u_beat);
+
+    float cnoise = drawCircle(vec2(st_noise * st.x, st_noise * st.y), vec2(sin(u_time/2.) * 0.25 + (2. * u_transition1) + 0.25 + (2. * u_transition1)), 0.05 + (5.*u_transition1));
+    color = mix(color, vec3(0.4784, 0.2314, 0.0275), cnoise * 2.);
+
+    vec2 rot = rotate2D(st, u_time / 6.);
+
+    float rl = 15.9;
+    float rh = 0.2;
+
+    // all rectangles that correspond to the percussive noise in the second half
+    float rect = drawRectangle(st, vec2(-1., -5.), vec2(rl, rh)) + drawRectangle(st, vec2(-1., -7.3), vec2(rl, rh)) + drawRectangle(st, vec2(-1., -9.5), vec2(rl, rh));
+    color = mix(color, vec3(0.123, 0.345, 0.567), rect * u_perc1);
+
+    vec2 rot1b = rotate2D(st, u_time / 6.);
+    float rect1b = drawRectangle(rot1b, vec2(-1., -5.), vec2(rl, rh)) + drawRectangle(rot1b, vec2(-1., -7.3), vec2(rl, rh)) + drawRectangle(rot1b, vec2(-1., -9.5), vec2(rl, rh));
+    color = mix(color, vec3(0.123, 0.345, 0.567), rect1b * u_perc1b);
+
+    vec2 rot1c = rotate2D(st, u_time / 6. + 10.);
+    float rect1c = drawRectangle(rot1c, vec2(-1., -5.), vec2(rl, rh)) + drawRectangle(rot1c, vec2(-1., -7.3), vec2(rl, rh)) + drawRectangle(rot1c, vec2(-1., -9.5), vec2(rl, rh));
+    color = mix(color, vec3(0.123, 0.345, 0.567), rect1c * u_perc1c);
+
+    float rect2 = drawRectangle(st, vec2(-6., -10.), vec2(rl, rh)) + drawRectangle(st, vec2(-6., -12.4), vec2(rl, rh)) + drawRectangle(st, vec2(-6., -15.6), vec2(rl, rh));
+    color = mix(color, vec3(0.4667, 0.0353, 0.0196), rect2 * u_perc2);
+
+    vec2 rot2b = rotate2D(st, u_time / 6. + 10.);
+    float rect2b = drawRectangle(rot2b, vec2(-6., -10.), vec2(rl, rh)) + drawRectangle(rot2b, vec2(-6., -12.4), vec2(rl, rh)) + drawRectangle(rot2b, vec2(-6., -15.6), vec2(rl, rh));
+    color = mix(color, vec3(0.4667, 0.0353, 0.0196), rect2b * u_perc2b);
+
+    vec2 rot2c = rotate2D(st, u_time / 6. + 20.);
+    float rect2c = drawRectangle(rot2c, vec2(-6., -10.), vec2(rl, rh)) + drawRectangle(rot2c, vec2(-6., -12.4), vec2(rl, rh)) + drawRectangle(rot2c, vec2(-6., -15.6), vec2(rl, rh));
+    color = mix(color, vec3(0.4667, 0.0353, 0.0196), rect2c * u_perc2c);
+
+    float si = sin(st.x*0.1+u_time/2.)*3.5 + cos(st.x + u_time / 3.); + sin(st.y + st.x * u_time / 2.);
+    float si2 = sin(st.x+u_time/2. + 30.)*5.5 + cos(st.x + u_time / 4.) + cos(st.y);
+    float siplot = plot(st,si, 0.8, 0.8) + plot(st, si2, 0.6, 0.3);
+
+    color = mix(color, vec3(0.0353* st.x * st.y * 0.2, 0.0078* st.x * st.y * 0.2, 0.4667* st.x * st.y * 0.2), siplot * u_sibeat);
+
+    gl_FragColor = vec4(color * f, 1.0);
+}
---
+++ sketch.js
@@ -0,0 +1,307 @@
+let musicShader;
+// these variables keep track of opacity of visual elements (correspond to audio)
+let beat = 0;
+let perc1 = 0;
+let perc1b = 0;
+let perc1c = 0;
+let perc2 = 0;
+let perc2b = 0;
+let perc2c = 0;
+let sibeat = 0;
+
+// these variables keep track of the zoom transition
+let transition1 = 0;
+let transition1State = "pre";
+
+function preload() {
+  musicShader = loadShader("shader.vert", "shader.frag");
+}
+
+function setup() {
+  // shaders require WEBGL mode to work
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  noStroke();
+}
+
+function draw() {
+  // shader() sets the active shader with our shader
+  shader(musicShader);
+
+  musicShader.setUniform("u_time", frameCount * 0.01);
+  musicShader.setUniform("u_resolution", [width, height]);
+
+  // bass note / circle
+  musicShader.setUniform("u_beat", beat);
+
+  // zoom transition
+  musicShader.setUniform("u_transition1", transition1);
+
+  // percussive lines
+  musicShader.setUniform("u_perc1", perc1);
+  musicShader.setUniform("u_perc1b", perc1b);
+  musicShader.setUniform("u_perc1c", perc1c);
+  musicShader.setUniform("u_perc2", perc2);
+  musicShader.setUniform("u_perc2b", perc2b);
+  musicShader.setUniform("u_perc2c", perc2c);
+  musicShader.setUniform("u_perc2c", perc2c);
+
+  // high drone
+  musicShader.setUniform("u_sibeat", sibeat);
+
+  // rect gives us some geometry on the screen
+  rect(0, 0, width, height);
+
+  // fade out elements gradually if currently visible
+  if (beat > 0) {
+    beat -= 0.01;
+  }
+  if (perc1 > 0) {
+    perc1 -= 0.1;
+  }
+  if (perc1b > 0) {
+    perc1b -= 0.1;
+  }
+  if (perc1c > 0) {
+    perc1c -= 0.1;
+  }
+  if (perc2 > 0) {
+    perc2 -= 0.1;
+  }
+  if (perc2b > 0) {
+    perc2b -= 0.1;
+  }
+  if (perc2c > 0) {
+    perc2c -= 0.1;
+  }
+  if (sibeat > 0) {
+    sibeat -= 0.005;
+  }
+
+  if (transition1State === "active") {
+    if (transition1 >= 1) {
+      transition1On = "complete";
+    } else {
+      if (transition1 < 0.1 || transition1 >= 0.8) {
+        // this is the zoom position passed to the shader
+        transition1 += 0.001;
+      } else if (transition1 >= 0.1 && transition1 < 0.9) {
+        transition1 += 0.005;
+      }
+    }
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function triggerTransition1() {
+  transition1State = "active";
+}
+
+// the end curtain
+let curtain = document.getElementById("curtain");
+
+function triggerEnd() {
+  curtain.style.display = "flex";
+}
+
+// tone js
+const startButton = document.getElementById("start-audio");
+
+startButton.addEventListener("click", async () => {
+  startButton.style.display = "none";
+  await Tone.start();
+  setTimeout(() => {
+    Tone.Transport.start();
+  }, 1000);
+  startButton.style.display = none;
+});
+
+// make some effects nodes
+const feedbackDelay = new Tone.FeedbackDelay("4n", 0.8);
+const dist = new Tone.Distortion(0.3);
+const reverb1 = new Tone.Reverb({ decay: 100, wet: 0.7 });
+const reverb2 = new Tone.Reverb({ decay: 100, wet: 0.8 });
+
+// plays in first half
+const synthA = new Tone.PolySynth(Tone.FMSynth).chain(
+  reverb1,
+  Tone.Destination
+);
+synthA.set({
+  harmonicity: 0.5,
+  modulationIndex: 5,
+  modulation: {
+    type: "square",
+  },
+  modulationEnvelope: {
+    attack: 0.5,
+    decay: 0,
+    sustain: 1,
+    release: 0.5,
+  },
+});
+
+//plays in first half
+const synthB = new Tone.AMSynth({
+  harmonicity: 0.25,
+  envelope: {
+    attack: 0.2,
+    decay: 0.01,
+    sustain: 1,
+    release: 0.8,
+  },
+  modulation: {
+    type: "square",
+  },
+  modulationEnvelope: {
+    attack: 0.5,
+    decay: 0,
+    sustain: 1,
+    release: 0.5,
+  },
+}).chain(feedbackDelay, dist, reverb2, Tone.Destination);
+
+//plays in second half - percussive
+const noiseSynth = new Tone.NoiseSynth().chain(feedbackDelay);
+
+//plays in second half
+const duoSynth = new Tone.DuoSynth({
+  vibratoAmount: 0.5,
+  vibratoRate: 0.3,
+  harmonicity: 2,
+  voice0: {
+    portamento: 0,
+    oscillator: {
+      type: "sine",
+    },
+    filterEnvelope: {
+      attack: 0.2,
+      decay: 0,
+      sustain: 1,
+      release: 100,
+    },
+    envelope: {
+      attack: 50,
+      decay: 0,
+      sustain: 1,
+      release: 300,
+    },
+  },
+  voice1: {
+    portamento: 0,
+    oscillator: {
+      type: "sine",
+    },
+    filterEnvelope: {
+      attack: 0.1,
+      decay: 0,
+      sustain: 1,
+      release: 100,
+    },
+    envelope: {
+      attack: 20,
+      decay: 0,
+      sustain: 1,
+      release: 500,
+    },
+  },
+}).chain(reverb2);
+
+// bass-y
+const part = new Tone.Part(
+  (time, value) => {
+    synthA.triggerAttackRelease(value.note, "16n", time, value.velocity);
+
+    // need to use this for drawing
+    Tone.Draw.schedule(() => {
+      beat = 1;
+      const bar = parseInt(Tone.Transport.position.split(":")[0]);
+      if (bar > 30 && transition1State === "pre") {
+        // zoom out when near the end of the half
+        triggerTransition1();
+      }
+    }, time);
+  },
+  [
+    { time: "0:0", note: "E3", velocity: 1.0 },
+    { time: "0:1", note: "E3", velocity: 0.6 },
+  ]
+);
+part.loop = 32;
+part.probability = 0.7;
+part.humanize = true;
+part.start(0);
+
+const partB = new Tone.Part(
+  (time, value) => {
+    // the value is an object which contains both the note and the velocity
+    synthB.triggerAttackRelease(value.note, "8n", time, value.velocity);
+  },
+  [
+    { time: "0:0:0", note: "B3", velocity: 0.6 },
+    { time: "0:0:2", note: "A4", velocity: 0.3 },
+    { time: "0:0:3", note: "B4", velocity: 0.5 },
+    { time: "0:1", note: "E#4", velocity: 0.6 },
+    { time: "0:2", note: "B4", velocity: 0.4 },
+    { time: "0:3:1", note: "A4", velocity: 0.4 },
+    { time: "0:3:2", note: "F#4", velocity: 0.6 },
+  ]
+);
+partB.loop = 32;
+partB.probability = 0.5;
+partB.humanize = true;
+partB.start(0);
+
+const partC = new Tone.Part(
+  (time, value) => {
+    // the value is an object which contains both the note and the velocity
+    noiseSynth.triggerAttackRelease("16n", time, value.velocity);
+
+    Tone.Draw.schedule(() => {
+      if (value.note === "C2") {
+        perc1 = 1;
+        setTimeout(() => {
+          perc1b = 1;
+        }, Tone.Time("4n").toMilliseconds());
+        setTimeout(() => {
+          perc1c = 1;
+        }, Tone.Time("4n").toMilliseconds() * 2);
+      } else {
+        perc2 = 1;
+        setTimeout(() => {
+          perc2b = 1;
+        }, Tone.Time("4n").toMilliseconds());
+        setTimeout(() => {
+          perc2c = 1;
+        }, Tone.Time("4n").toMilliseconds() * 2);
+      }
+    }, time);
+  },
+  [
+    { time: "0:0:0", note: "C2", velocity: 0.1 },
+    { time: "0:1:2", note: "G2", velocity: 0.1 },
+  ]
+);
+partC.loop = 32;
+partC.probability = 0.3;
+partC.start("32m"); //
+
+const partD = new Tone.Part(
+  (time, value) => {
+    duoSynth.triggerAttackRelease(value.note, "1m", time, value.velocity);
+    Tone.Draw.schedule(() => {
+      sibeat = 1;
+      const bar = parseInt(Tone.Transport.position.split(":")[0]);
+      if (bar > 62) {
+        triggerEnd();
+      }
+    }, time);
+  },
+  [{ time: "0:0:0", note: "A4", velocity: 0.6 }]
+);
+partD.loop = 32;
+partD.probability = 0.2;
+partD.humanize = true;
+partD.start("32m");
